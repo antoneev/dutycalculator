9245f411e6d151ada2e79168e401ff0aca2ed769
Timothy Itodo <itodotimothy6@gmail.com>
2019-08-26 16:33:58 -0500

    Fix footer

--- js/script.js
+++ js/script.js
@@ -14,4 +14,13 @@ $(document).ready(function () {
         option.value = keys[i];
         select.appendChild(option);
     }
+
+    // Footer
+    var window_height = window.innerHeight;
+    var navbar_height = document.getElementById("nav_bar").offsetHeight;
+    var container_height = document.getElementById("main_container").offsetHeight;
+    var footer_height = document.getElementById("footer").offsetHeight;
+    var height = window_height - (navbar_height + 20 + container_height + footer_height) ;
+    console.log(height);
+    $('#footer').css('margin-top',height);
 });
