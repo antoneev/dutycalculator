5e2371d29493d8a45921ff467fe86eb191e35176
Timothy Itodo <itodotimothy6@gmail.com>
2019-09-03 20:03:32 -0500

    Make popovers trigger to be hover

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 // Popover 
 $(document).ready(function () {
-    // Initialize bootstrap popover
-    $('[data-toggle="popover"]').popover();
+    $(".pop").popover({ trigger: "manual", html: true, animation: false })
+        .on("mouseenter", function () {
+            var _this = this;
+            $(this).popover("show");
+            $(".popover").on("mouseleave", function () {
+                $(_this).popover('hide');
+            });
+        }).on("mouseleave", function () {
+            var _this = this;
+            setTimeout(function () {
+                if (!$(".popover:hover").length) {
+                    $(_this).popover("hide");
+                }
+            }, 300);
+        });
     fill_product_category_data();
     fill_levy_category_data();
     stick_footer();
